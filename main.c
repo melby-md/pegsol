12f6c3b01c28194de7c087a71a62853d52693bc6
Pedro Debs <pedrodebs1@gmail.com>
2023-07-03 00:32:32 -0300

    added victory/defeat condition

--- main.c
+++ main.c
@@ -3,12 +3,6 @@
 
 #include "SDL.h"
 
-// window dimension
-static const int size = 512;
-static const int tile = size / 7;
-static const int half_tile = size / 14;
-static const int radius = size / 26;
-
 // https://gist.github.com/Gumichan01/332c26f6197a432db91cc4327fcabb1c
 static int
 draw_circle(SDL_Renderer *renderer, int x, int y, int rad)
@@ -55,6 +49,60 @@ draw_circle(SDL_Renderer *renderer, int x, int y, int rad)
     return status;
 }
 
+static int
+locked(uint8_t *pegs)
+{
+	int a, b, c;
+	int sum = 0;
+
+	for (int l = 0; l < 7; l ++) {
+		if (l > 1 && l < 5) {
+			for (int i = 0; i < 5; i++) {
+				a = (pegs[l] >> (2 + i)) & 1;
+				b = (pegs[l] >> (1 + i)) & 1;
+				c = (pegs[l] >> i) & 1;
+				if (b && (b || c) && (a != c)) {
+					return 0;
+				}
+			}
+		} else {
+			a = (pegs[l] >> 4) & 1;
+			b = (pegs[l] >> 3) & 1;
+			c = (pegs[l] >> 2) & 1;
+			if (b && (b || c) && (a != c)) {
+				return 0;
+			}
+		}
+	}
+
+	for (int l = 0; l < 7; l ++) {
+		if (l > 1 && l < 5) {
+			sum += ((pegs[1] >> l) & 1) + ((pegs[0] >> l) & 1);
+			for (int i = 0; i < 5; i++) {
+				a = (pegs[i + 2] >> l) & 1;
+				b = (pegs[i + 1] >> l) & 1;
+				c = (pegs[i] >> l) & 1;
+				if (b && (b || c) && (a != c)) {
+					return 0;
+				}
+				sum += a;
+			}
+		} else {
+			a = (pegs[4] >> l) & 1;
+			b = (pegs[3] >> l) & 1;
+			c = (pegs[2] >> l) & 1;
+			if (b && (b || c) && (a != c)) {
+				return 0;
+			}
+			sum += a + b + c;
+		}
+	}
+	if (sum == 1)
+		return -1;
+	else
+		return 1;
+}
+
 static bool
 is_valid_movement(uint8_t *pegs, int peg, int dest, int *deleted)
 {
@@ -83,14 +131,47 @@ is_valid_movement(uint8_t *pegs, int peg, int dest, int *deleted)
 	return false;
 }
 
+static void
+render(SDL_Renderer *renderer, uint8_t *pegs, int size, int selected)
+{
+	int tile = size / 7;
+	int half_tile = size / 14;
+	int radius = size / 26;
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderClear(renderer);
+
+	for (int y = 0; y < 7; y++)
+		for (int x = 0; x < 7; x++) {
+			if ((y < 2 || y > 4) && (x < 2 || x > 4)) {
+				continue;
+			}
+
+			int i = y*7 + x;
+			int rad = radius;
+			if (pegs[y] & 1 << x) {
+				if (i == selected)
+					SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+				else
+					SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+			} else {
+				SDL_SetRenderDrawColor(renderer, 127, 127, 127, 255);
+				rad /= 2;
+			}
+			draw_circle(renderer, x*tile+half_tile, y*tile+half_tile, rad);
+		}
+
+	SDL_RenderPresent(renderer);
+}
+
 int
 main(int argc, char *args[])
 {
-	uint8_t pegs[7] = {127, 127, 127, 127, 127, 127, 127};
+	uint8_t pegs[7] = {28, 28, 127, 119, 127, 28, 28};
+	int size = 512;
+	int tile = size / 7;
+	int half_tile = size / 14;
+	int radius = size / 26;
 	int selected = -1;
-
-	pegs[3] &= ~(1 << 3);
-
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		SDL_ShowSimpleMessageBox(
@@ -101,7 +182,7 @@ main(int argc, char *args[])
 	}
 
 	SDL_Window *window = SDL_CreateWindow(
-		"Solitaire Peg",
+		"Peg Solitaire",
 		SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED,
 		size,
@@ -111,9 +192,9 @@ main(int argc, char *args[])
 
 	if (window == NULL) {
 		SDL_ShowSimpleMessageBox(
-			SDL_MESSAGEBOX_ERROR, "SDL_Init()", SDL_GetError(), 0
+			SDL_MESSAGEBOX_ERROR, "SDL_CreateWindow()", SDL_GetError(), 0
 		);
-		SDL_Log("SDL_Init(): %s", SDL_GetError());
+		SDL_Log("SDL_CreateWindow(): %s", SDL_GetError());
 		return 1;
 	}
 
@@ -121,38 +202,16 @@ main(int argc, char *args[])
 
 	if (renderer == NULL) {
 		SDL_ShowSimpleMessageBox(
-			SDL_MESSAGEBOX_ERROR, "SDL_Init()", SDL_GetError(), 0
+			SDL_MESSAGEBOX_ERROR, "SDL_CreateRenderer()", SDL_GetError(), window
 		);
-		SDL_Log("SDL_Init(): %s", SDL_GetError());
+		SDL_Log("SDL_CreateRenderer(): %s", SDL_GetError());
 		return 1;
 	}
 
 	SDL_Event e;
 	bool quit = false;
 	while(!quit) {
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		SDL_RenderClear(renderer);
-
-		int i = 0;
-		for (int y = 0; y <= 6; y++)
-			for (int x = 0; x <= 6; x++) {
-				if ((y > 1 && y < 5) || (x > 1 && x <5)) {
-					int rad = radius;
-					if (pegs[y] & 1 << x) {
-						if (i == selected)
-							SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-						else
-							SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-					} else {
-						SDL_SetRenderDrawColor(renderer, 127, 127, 127, 255);
-						rad /= 2;
-					}
-					draw_circle(renderer, x*tile+half_tile, y*tile+half_tile, rad);
-				}
-				i++;
-			}
-
-		SDL_RenderPresent(renderer);
+		render(renderer, pegs, size, selected);
 		while (SDL_PollEvent(&e)) {
 			switch (e.type) {
 			case SDL_QUIT:
@@ -161,31 +220,57 @@ main(int argc, char *args[])
 			case SDL_MOUSEBUTTONDOWN:
 				int x, y, x_s, y_s, n;
 				SDL_GetMouseState(&x, &y);
-				if ((y > tile*2 && y < tile*5) || (x > tile*2 && x < tile*5)) {
-					x_s = x -x%tile + half_tile;
-					y_s = y -y%tile + half_tile;
-					if ((x*x - 2*x*x_s + x_s*x_s) + (y*y - 2*y*y_s + y_s*y_s) < radius*radius) {
-						int x_n = x/tile;
-						int y_n = y/tile;
-						n = y_n*7 + x_n;
 
-						if (pegs[y_n] & 1 << x_n) {
-							selected = n;
-							continue;
-						}
-
-						int deleted;
-						if (is_valid_movement(pegs, selected, n, &deleted)) {
-							pegs[deleted/7] &= ~(1 << deleted%7);
-							pegs[selected/7] &= ~(1 << selected%7);
-							pegs[n/7] |= 1 << n%7;
-						} else
-							selected = -1;
-
-					} else
-						selected = -1;
-				} else
+				if (!((y > tile*2 && y < tile*5) || (x > tile*2 && x < tile*5))) {
 					selected = -1;
+					continue;
+				}
+
+				x_s = x -x%tile + half_tile;
+				y_s = y -y%tile + half_tile;
+				if ((x*x - 2*x*x_s + x_s*x_s) + (y*y - 2*y*y_s + y_s*y_s) >= radius*radius) {
+					selected = -1;
+					continue;
+				}
+
+				int x_n = x/tile;
+				int y_n = y/tile;
+				n = y_n*7 + x_n;
+
+				if (pegs[y_n] & 1 << x_n) {
+					selected = n;
+					continue;
+				}
+
+				int deleted;
+				if (!is_valid_movement(pegs, selected, n, &deleted)) {
+					selected = -1;
+					continue;
+				}
+
+				pegs[deleted/7] &= ~(1 << deleted%7);
+				pegs[selected/7] &= ~(1 << selected%7);
+				pegs[n/7] |= 1 << n%7;
+
+				int res;
+				if (!(res = locked(pegs))) {
+					continue;
+				}
+
+				render(renderer, pegs, size, selected);
+				char *msg;
+				if (res == -1)
+					msg = "You Win!";
+				else
+					msg = "You Lose";
+
+				SDL_ShowSimpleMessageBox(
+					SDL_MESSAGEBOX_INFORMATION,
+					"END",
+					msg,
+					window	
+				);
+				quit = true;
 			}
 		}
 	}
